331949199ead3e35b59e3dc81ae0ad9c6c78f339
Mariana <bymarianamoreira@gmail.com>
2025-01-19 18:08:29 -0300

    add day 10

---
+++ day 10/main.py
@@ -0,0 +1,61 @@
+import re
+from art import logo
+
+print(logo)
+
+def is_valid_number(input_string):
+    return bool(re.match(r'^-?\d+(\.\d+)?$', input_string))
+
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1, n2):
+    return n1 - n2
+def multiply(n1, n2):
+    return n1 * n2
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+def calculator():
+    should_accumulate = True
+
+    number1 = input("What's the first number?: ")
+    if not is_valid_number(number1):
+        print("Invalid number, please try again.")
+        exit()
+    number1 = float(number1)
+
+    while should_accumulate:
+        operator = input("+\n-\n*\n/\nPick an operation: ")
+        if operator not in operations:
+            print("Invalid operator, please try again.")
+            exit()
+
+        number2 = input("What's the next number?: ")
+        if not is_valid_number(number2):
+            print("Invalid number, please try again.")
+            exit()
+        number2 = float(number2)
+
+        answer = operations[operator](number1, number2)
+        
+        print(f"{number1} {operator} {number2} = {answer}\n")
+
+        new_calculation = input(f"Type 'y' to continue calculating with {answer} or type 'n' to start a new calculation: ")
+        if new_calculation == 'y':
+            number1 = answer
+        elif new_calculation == 'n':
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+        else:
+            print("Invalid input, please try again.\n")
+            calculator()
+
+calculator()
