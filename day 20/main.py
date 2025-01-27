acfffe668921de79f03f87fb4929b1e8a7d21b8d
Mariana <bymarianamoreira@gmail.com>
2025-01-27 20:17:41 -0300

    add day 21

--- day 20/main.py
+++ day 20/main.py
@@ -1,15 +1,19 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
-dimensios = 600
-screen.setup(dimensios, dimensios)
+dimensions = 600
+screen.setup(dimensions, dimensions)
 screen.bgcolor("pink")
 screen.title("Cute Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+score = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,8 +25,20 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        score.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            Scoreboard.game_over()
 
 screen.exitonclick() 
---
+++ day 20/scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("purple")  
+        self.penup()              
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", False, align="center", font=("Arial", 18, "normal"))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", False, align="center", font=("Arial", 18, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
