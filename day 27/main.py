76f6943c90805a4330ca05d5853451a5de28e325
Mariana <bymarianamoreira@gmail.com>
2025-02-03 16:51:05 -0300

    add day 27

--- day 27/main.py
+++ day 27/main.py
@@ -1,24 +1,41 @@
 from tkinter import *
 
+#(row 0, column 1) quantas miles
+#(0, 2) miles
+#(1,0) is equal to
+#(1, 1) resultado
+#(1, 2) Km
+#(2, 1) button calculate
+
+def converting_to_km():
+    miles = float(miles_input.get())
+    km = miles * 1.60934
+    result_label.config(text=f"{km:.2f}")
+
+#Window configurations
 window = Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
+window.title("Mile to Km Converter")
+window.config(padx=20, pady=20)
 
-my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
+#Labels configurations
+miles_label = Label(text="Miles")
+miles_label.grid(row=0, column=2)
 
-def button_clicked():
-    new_text = input.get()
-    my_label.config(text=new_text)
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(row=1, column=0)
 
-button = Button(text="Click me", command=button_clicked)
-button.pack()
+result_label = Label(text="0")
+result_label.grid(row=1, column=1)
 
-input = Entry(width=10)
-input.pack()
-input.get()
+km_label = Label(text="Km")
+km_label.grid(row=1, column=2)
 
+#Entries configurations
+miles_input = Entry(width=7)
+miles_input.grid(row=0, column=1)
 
-
+#Button configuration
+button = Button(text="Calculate", command=converting_to_km)
+button.grid(row=2, column=1)
 
 window.mainloop()
