0ebfb874f9b474c2d864ed8b76974d8ea0b2dc72
Mariana <bymarianamoreira@gmail.com>
2025-02-20 20:27:35 -0300

    add day 45

--- day 45/main.py
+++ day 45/main.py
@@ -4,5 +4,13 @@ from bs4 import BeautifulSoup
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
+response = requests.get(URL)
+web_page = response.text
+soup = BeautifulSoup(web_page, "html.parser")
+movies = [title.getText() for title in soup.find_all(name="h3", class_="title")]
 
+movies.reverse()
 
+with open("day 45/movies.txt", "w", encoding="utf-8") as file:
+    for title in movies:
+        file.write(f"{title}\n")
