dccd96a83e61b2c71db8c15d53b5913e108edd80
Mariana <bymarianamoreira@gmail.com>
2025-02-23 22:11:53 -0300

    day 48: first task completed

--- day 48/main.py
+++ day 48/main.py
@@ -1,10 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome()
-driver.get("https://pypi.org/project/selenium/")
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.python.org/")
 
-driver.close()
+event_times = driver.find_elements(By.CSS_SELECTOR, value=".event-widget time")
+event_names = driver.find_elements(By.CSS_SELECTOR, value=".event-widget li a")
+
+events = {number: {"time": event_times[number].text, "name": event_names[number].text} for number in range(len(event_times))}
+print(events)
+
+#driver.close()
 driver.quit()
